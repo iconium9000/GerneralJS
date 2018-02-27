c9a00ef220c111ec2547c3ea7719950b3259a169
John FitzGerald (MBA) <iconium9000@gmail.com>
2018-02-27 14:43:04 -0800

    v0.8.0
    moved around folders

---
+++ projects/0004_mg/0001/game.js
@@ -0,0 +1,19 @@
+PROJECT_NAME = 'Maze Game v4'
+log('init game.js', PROJECT_NAME)
+
+GAME_MSG = (key, sndr, rcvr, msg) => {
+
+}
+
+
+
+GAME_SRVR_INIT = () => {
+  log('init game srvr')
+}
+GAME_CLNT_INIT = () => {
+  log('init game clnt')
+}
+
+GAME_TICK = () => {
+  
+}
