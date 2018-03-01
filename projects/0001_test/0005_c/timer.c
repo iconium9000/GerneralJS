69f980916651478e702e720a61df04a7fc9ae26d
John FitzGerald (MBA) <iconium9000@gmail.com>
2018-02-28 17:46:46 -0800

    v0.8.1.1
    minor changes

--- projects/0001_test/0005_c/timer.c
+++ projects/0001_test/0005_c/timer.c
@@ -1,62 +1,69 @@
-/*
-  Write a timer program that accepts a number of seconds to count down
-  from as an argument.
+// read ASCII password (Unicode not supported)
 
-  The program counts down from that number until 0, decerementing every
-  second.
-
-  It displays the time remaining.
-
-  It reprograms the terminal driver to support pausing the timer.  It does
-  this by reprogramming terminal settings VSTART and VSTOP to S and Q.
-  S - starts the timer / Q - pauses the timer.
-
-  When the user types Q or S, that is not shown on the screen, so the
-  program should disable echoing while it's running as well.
-  ---
-  The program supports the following commands :
-  p - pauses the timer
-  u - unpauses the timer
-*/
+// uses source code from the example in section 17.7 of the GNU C Library Reference Manual,
+// however that example seems to have a bug that I have corrected (I use write instead
+// of putchar as otherwise non-canonical mode does not work)
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <termios.h>
 #include <unistd.h>
+#include <termios.h>
+#include <ctype.h>
 
-int main(int ac, char* args[])
+
+/* Use this variable to remember original terminal attributes. */
+struct termios saved_attributes;
+
+void
+reset_input_mode (void)
 {
-    struct termios old_settings, new_settings;
+  tcsetattr (STDIN_FILENO, TCSANOW, &saved_attributes);
+}
 
-    // get the terminal settings
-    tcgetattr(0, &old_settings);
-    tcgetattr(0, &new_settings);
+void
+set_input_mode (void)
+{
+  struct termios tattr;
+  char *name;
 
-    // update them
-
-    // turn off echoing
-    new_settings.c_lflag &= ~ECHO;
-
-    // change VSTART to 'u'
-    new_settings.c_cc[VSTART] = 'u';
-
-    // change VSTOP to 'p'
-    new_settings.c_cc[VSTOP] = 'p';
-
-    // send them back
-    tcsetattr(0, TCSANOW, &new_settings);
-
-    // do the countdown thing
-    int seconds = atoi(args[1]);
-
-
-    while(seconds >= 0)
+/* Make sure stdin is a terminal. */
+  if (!isatty (STDIN_FILENO))
     {
-        printf("%i\n", seconds);
-        sleep(1);
-        seconds--;
+      fprintf (stderr, "Not a terminal.\n");
+      exit (EXIT_FAILURE);
     }
 
-    // restore original settings
-    tcsetattr(0, TCSANOW, &old_settings);
+/* Save the terminal attributes so we can restore them later. */
+  tcgetattr (STDIN_FILENO, &saved_attributes);
+  atexit (reset_input_mode);
+
+/* Set the funny terminal modes. */
+  tcgetattr (STDIN_FILENO, &tattr);
+  tattr.c_lflag &= ~(ICANON | ECHO);	/* Clear ICANON and ECHO. */
+  tattr.c_cc[VMIN] = 1;
+  tattr.c_cc[VTIME] = 0;
+  tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
+}
+
+int
+main ()
+{
+  int i = 0;
+  char c, password[100], asterisk = '*';
+
+
+  set_input_mode ();
+
+  while (read (STDIN_FILENO, &c, 1) && (isalnum (c) || ispunct (c))
+	 && i < sizeof (password) - 2)
+    {
+      password[i++] = c;
+      write (STDOUT_FILENO, &asterisk, 1);
+    }
+
+  password[i] = 0;
+
+  printf ("\nPassword was: [%s]\n", password);
+
+  return EXIT_SUCCESS;
 }
