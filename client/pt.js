6f9dd70f87805cc5deeda4f02da853d06b426153
John FitzGerald <iconium9000@gmail.com>
2018-03-26 09:01:37 -0700

    v0.8.6

--- client/pt.js
+++ client/pt.js
@@ -453,6 +453,13 @@ PT.vcc = (s,f,l) => {
   }
 }
 
+/*
+
+types: vec1, vec2, vec3, vec\X, mat3_3, mat4_4, mat\X_\Y, str, bol
+
+
+*/
+
 PT.fcc = function() {
   var temp = ()=>{}
   var del_tok = {
@@ -460,47 +467,74 @@ PT.fcc = function() {
     '\t': true
   }
   var tokens = {
-    ' ': ' ',
-    '(': '(',
-    ')': ')',
-    '+': '+',
-    '-': '-',
-    '*': '*',
-    '"': '"',
-    "`": "`",
-    "'": "'",
-    '%': '%',
-    '!': '!',
-    '^': '^',
-    '{': '{',
-    '}': '}',
-    ';': ';',
-    '?': '?',
-    ':': ':',
-    '[': '[',
-    ']': ']',
-    '++': '++',
-    '--': '--',
-    '//': '//',
-    '/*': '/*',
-    '*/': '*/',
-    '\n': '\n',
-    '\t': '\t',
-    '$': '$',
-    '#': '#',
-    '&': '&',
-    '#': '#',
-    '=': '=',
-    '~': '~',
-    ',': ',',
-    '&&': '&&',
-    '||': '||',
-    '|': '|'
+    ' ': true,
+    '(': true,
+    ')': true,
+    '+': true,
+    '-': true,
+    '*': true,
+    '"': true,
+    "`": true,
+    "'": true,
+    '%': true,
+    '!': true,
+    '^': true,
+    '{': true,
+    '}': true,
+    ';': true,
+    '?': true,
+    ':': true,
+    '[': true,
+    ']': true,
+    '++': true,
+    '--': true,
+    '//': true,
+    '/*': true,
+    '*/': true,
+    '\n': true,
+    '\t': true,
+    '$': true,
+    '#': true,
+    '&': true,
+    '#': true,
+    '=': true,
+    '==': true,
+    '!=': true,
+    '<': true,
+    '>': true,
+    '>=': true,
+    '<=': true,
+    '~': true,
+    ',': true,
+    '&&': true,
+    '||': true,
+    '|': true,
   }
-  var is_stat = {
+  var is_scp = {
     '{}':true,
     '()':true,
-    '[]':true
+    '[]':true,
+    'abs':true,
+    'stat':true,
+  }
+  var is_val = {
+    'fun': true,
+    'str': true,
+    'num': true,
+    'wrd': true,
+    '{}': true,
+    '[]': true,
+    '()': true,
+    'abs': true
+  }
+  var is_dlm = {
+    '\n': true,
+    ',': true,
+    ';': true,
+  }
+
+  var tok_ops = {
+
   }
 
   function match(s,ary) {
@@ -509,17 +543,18 @@ PT.fcc = function() {
   }
   function parseString(string) {
     var word = ''
-    var ans = []
+    var ans = ['{}']
 
     var line = 0
     var char = 0
 
     var push = () => {
+      var line_char = `${line}:${char}`
       if (word.length == 0) return
       else if (del_tok[word]) return
-      else if (tokens[word]) ans.push(['tok',word,line,char])
-      else if (isNaN(parseFloat(word))) ans.push(['wrd',word,line,char])
-      else ans.push(['num',parseFloat(word),line,char])
+      else if (tokens[word]) ans.push(['tok',word,line_char])
+      else if (isNaN(parseFloat(word))) ans.push(['wrd',word,line_char])
+      else ans.push(['num',parseFloat(word),line_char])
       if (word == '\n') {
         ++line
         char = 0
@@ -544,58 +579,360 @@ PT.fcc = function() {
     push()
     return ans
   }
-  function replace(parse, s, e, r, reqe) {
-    var stack = [[]]
-    var p
-
-    // else if (reqe && p[1] == e) throw `unexpected '${e}' at ${p[2]}:${p[3]}`
-
+  function replace(parse, s, e, r, reqe, del) {
+    var stack = []
+    var ans = []
+    var p = null
     for (var i in parse) {
       p = parse[i]
-      if (is_stat[parse[0]]) p = replace(p,s,e,r)
+      var p1 = p[1]
 
-      var l = stack.pop()
-      
-      if (l == null)
-        throw `unexpected '${e}' before ${p[2]}:${p[3]}`
-
-      if (p[1] == s) {
-        var t = [r]
-        l.push(t)
-        stack.push(l,t)
+      if (i == 0) ans.push(p)
+      else if (is_scp[p[0]]) ans.push(replace(p,s,e,r,reqe))
+      else if (p1 == s && !(s == e && stack.length)) {
+        stack.push(ans)
+        ans = [r]
       }
-      else if (p[1] != e) {
-        l.push(p)
-        stack.push(l)
+      else if (p1 == e) {
+        var temp = stack.pop()
+        if (temp) {
+          del || temp.push(ans)
+          ans = temp
+        }
+        else if (reqe) throw `unexpected '${e}' at ${p[2]}`
+        else ans.push(p)
+      }
+      else ans.push(p)
+    }
+
+    if (stack.length) throw `expected '${e}' after ${p[2]}`
+
+    return ans
+  }
+  function parseStat(parse) {
+    var p0 = parse[0]
+    if (p0 == '()' || p0 == '[]' || p0 == '{}') {
+      var ans = [p0]
+
+      var stat = ['stat']
+      for (var i = 1; i < parse.length; ++i) {
+        var p = parseStat(parse[i])
+
+        if (is_dlm[p[1]]) {
+          stat.length>1 && ans.push(stat)
+          stat = ['stat']
+        }
+        else stat.push(p)
+      }
+
+      stat.length>1 && ans.push(stat)
+
+      if (ans.length == 2) {
+        ans = ans[1]
+        ans[0] = p0
+      }
+      return ans
+    }
+    return parse
+  }
+  function parseVal(parse) {
+    if (!is_scp[parse[0]]) return parse
+
+    var ans = []
+    var next = parseVal(parse[parse.length-1])
+    for (var i = parse.length-2; i >= 0; --i) {
+      var prev = parseVal(parse[i])
+      if (is_val[next[0]] && is_val[prev[0]]) {
+        next = ['fun', prev, next]
+      }
+      else {
+        ans.splice(0,0,next)
+        next = prev
       }
     }
-    if (stack.length > 1) throw `expected '${e}' at ${p[2]}:${p[3]}`
-    else if (stack.length) return stack.pop()
-    else `unexpected '${s}' before ${p[2]}:${p[3]}`
+    ans.splice(0,0,next)
+    return ans
+  }
+  function parsePrefx(parse,toks) {
+    var p0 = parse[0]
+    if (p0 == 'fun') {
+      for (var i = 1; i < parse.length; ++i)
+        parse[i] = parsePrefx(parse[i],toks)
+    }
+    else if (is_scp[p0]) {
+      var ans = []
+      var next = parsePrefx(parse[parse.length-1],toks)
+      for (var i = parse.length-2;i>=0;--i) {
+        var prev = parsePrefx(parse[i],toks)
+        var tok = toks[prev[1]]
+        if (tok && is_val[next[0]] && (tok==1 || i <= 1 || !is_val[parse[i-1][0]])) {
+          next = ['fun',['prefx',prev[1]],next]
+        }
+        else {
+          ans.splice(0,0,next)
+          next = prev
+        }
+      }
+      ans.splice(0,0,next)
+      return ans
+    }
+    return parse
+  }
+  function parsePstfx(parse,toks) {
+    var p0 = parse[0]
+    var len = parse.length
+    if (p0 == 'fun') {
+      for (var i = 1; i < len; ++i)
+        parse[i] = parsePstfx(parse[i],toks)
+    }
+    else if (is_scp[p0]) {
+      var ans = []
+      var prev = parsePstfx(parse[0],toks)
+      for (var i = 1; i < len; ++i) {
+        var next = parsePstfx(parse[i],toks)
+        var tok = toks[next[1]]
+        if (tok && is_val[prev[0]] && (tok==1 || i > len-2 || !is_val[parse[i+1][0]])) {
+          prev = ['fun',['pstfx',next[1]], prev]
+        }
+        else {
+          ans.push(prev)
+          prev = next
+        }
+      }
+      ans.push(prev)
+      return ans
+    }
+    return parse
+  }
+  function parseMidfx_LfRt(parse,toks) {
+    if (!parse) return null
+
+    var p0 = parse[0]
+    var len = parse.length
+
+    if (is_scp[p0] || p0 == 'fun')
+      for (var i = 1; i < len; ++i)
+        parse[i] = parseMidfx_LfRt(parse[i],toks)
+
+    if (is_scp[p0]) {
+
+      var ans = []
+      var lexA = parse[0]
+      var lexB = parse[1]
+
+      for (var i = 2; i < len; ++i) {
+        var lexC = parse[i]
+
+        if (toks[lexB[1]] && lexA && is_val[lexA[0]] && is_val[lexC[0]]) {
+          lexB = ['fun',['midfx',lexB[1]],lexA,lexC]
+          lexA = null
+        }
+        else {
+          lexA && ans.push(lexA)
+
+          lexA = lexB;
+          lexB = lexC;
+        }
+      }
+
+      lexA && ans.push(lexA)
+      lexB && ans.push(lexB)
+      return ans
+    }
+    return parse
+  }
+  function parseMidfx_RtLf(parse,toks) {
+    if (!parse) return null
+
+    var p0 = parse[0]
+    var len = parse.length
+
+    if (is_scp[p0] || p0 == 'fun')
+    for (var i = 1; i < len; ++i)
+      parse[i] = parseMidfx_RtLf(parse[i],toks)
+
+    if (is_scp[p0]) {
+
+      var ans = []
+      var lexC = parse[len-1]
+      var lexB = parse[len-2]
+
+      for (var i = len-3; i >= 0; --i) {
+        var lexA = parse[i]
+
+        if (toks[lexB[1]] && lexC && is_val[lexA[0]] && is_val[lexC[0]]) {
+          lexB = ['fun',['midfx',lexB[1]],lexA,lexC]
+          lexC = null
+        }
+        else {
+          lexC && ans.splice(0,0,lexC)
+
+          lexC = lexB;
+          lexB = lexA;
+        }
+      }
+
+      lexC && ans.splice(0,0,lexC)
+      lexB && ans.splice(0,0,lexB)
+      return ans
+    }
+    return parse
+  }
+  function parseConOp_helper(parse,i) {
+    var tokA = parse[i+1]
+
+    if (i > parse.length-2 || tokA[0] != 'tok' || tokA[1] != '?') return
+
+    var err = `unexpected '?' at ${tokA[2]}`
+
+    if (i > parse.length-4) throw err
+
+    var valA = parse[i]
+    if (!is_val[valA[0]]) throw err
+
+    parseConOp_helper(parse,i+2)
+    var valB = parse[i+2]
+    if (!is_val[valB[0]]) throw err
+
+    var tokB = parse[i+3]
+    if (tokB[0] != 'tok' || tokB[1] != ':') throw `expected ':' after ${tokA[2]}`
+
+    parseConOp_helper(parse,i+4)
+    var valC = parse[i+4]
+    if (!is_val[valC[0]]) throw err
+
+    parse.splice(i,5,['fun',['conop','?'],valA,valB,valC])
+  }
+  function parseConOp(parse) {
+    if (!parse) return null
+
+    var p0 = parse[0]
+    var len = parse.length
+
+    if (is_scp[p0] || p0 == 'fun')
+      for (var i = 1; i < len; ++i)
+        parse[i] = parseConOp(parse[i])
+
+    if (is_scp[p0])
+      for (var i = 0; i < parse.length; ++i)
+        parseConOp_helper(parse,i)
+    return parse
+  }
+  function parseRmvStat(parse) {
+    var p0 = parse[0]
+
+    if (p0 == 'stat') {
+      if (parse.length != 2) throw `stat syntax error, unhandled character`
+      else return parse[1]
+    }
+    else if (is_scp[p0] || p0 == 'fun') {
+      for (var i = 1; i < parse.length; ++i)
+        parse[i] = parseRmvStat(parse[i])
+    }
+
+    return parse
+  }
+  function parseFinal(parse,scp,src) {
+    var p0 = parse[0]
+    var p1 = parse[1]
+    var p2 = parse[2]
+    // log(parse)
+    var val = {}
+    val.scp = scp
+    val.src = src
+    val.type = p0
+    val.is_scp = false
+    if (p0 == '{}') {
+      for (var i = 1; i < parse.length; ++i) parse[i]
+    }
+    else if (p0 == 'abs') {
+      val.args = []
+      val.type = 'fun'
+      val.val = 'abs'
+      for (var i = 1; i < parse.length; ++i) val.args.push(parseFinal(parse[i],val,val))
+    }
+    else if (is_scp[p0]) {
+      val.stats = []
+      for (var i = 1; i < parse.length; ++i) val.stats.push(parseFinal(parse[i],scp,val))
+    }
+    else if (p0 == 'fun') {
+      val.args = []
+
+      var p10 = p1[0]
+      var p11 = p1[1]
+      var start = 1
+      val.val = 'act'
+
+      if (p10 == 'midfx' || p10 == 'prefx' || p10 == 'pstfx' || p10 == 'conop') {
+        val.val = p10 + p11
+        start = 2
+      }
+
+      for (var i = start; i < parse.length; ++i) val.args.push(parseFinal(parse[i],scp,val))
+    }
+    else if (p0 == 'num' || p0 == 'str' || p0 == 'wrd') {
+      val.val = p1
+      val.loc = p2
+    }
+    else throw `invalid identifier '${p0}'`
+
+    return val
   }
 
-  function printParse(parse) {
-    var s = ''
-    for(var c in parse) s += parse[c][1] == ';' ? '\n' : parse[c][1] + ' '
-    log(s)
+  function symbleTable(parse,table) {
+    var p0 = parse[0]
+    if (!is_scp[p0] && p0 != 'fun') return
+
+    for (var i = 1; i < parse.length; ++i) {
+      var p = parse[i]
+      if (p[0] == 'wrd') {
+        var t = table[p[1]]
+        if (!t) t = table[p[1]] = []
+
+        t.push([parse,i,p])
+      }
+      else symbleTable(p,table)
+    }
   }
 
   return function(string) {
 
-    var parse = parseString(string)
+    // syntax pass
+    var parse = parseString(string + '\n')
+    {
+      parse = replace(parse, "'","'", 'str')
+      parse = replace(parse, '"','"', 'str')
+      parse = replace(parse, '`','`', 'str')
+      parse = replace(parse, '//', '\n', 'com',false,true)
+      parse = replace(parse, '/*', '*/', 'com',false,true)
+      parse = replace(parse, '(',')', '()', true)
+      parse = replace(parse, '[',']', '[]', true)
+      parse = replace(parse, '{','}', '{}', true)
+      parse = replace(parse, '|', '|', 'abs', true)
+      parse = parseStat(parse)
+      parse = parsePrefx(parse,{'-':2,'+':2,'++':1,'--':1,'!':1,'~':1})
+      parse = parsePstfx(parse,{'++':1,'--':1})
+      parse = parseVal(parse)
+      parse = parseMidfx_RtLf(parse,{'^':1})
+      parse = parseMidfx_LfRt(parse,{'*':1,'/':1,'%':1})
+      parse = parseMidfx_LfRt(parse,{'+':1,'-':1})
+      parse = parseMidfx_LfRt(parse,{'&&':1,'||':1})
+      parse = parseMidfx_LfRt(parse,{'==':1,'>':1,'<':1,'>=':1,'<=':1,'!=':1})
+      parse = parseConOp(parse)
+      parse = parseMidfx_RtLf(parse,{'=':1})
+      parse = parseRmvStat(parse)
+
+      log(parseFinal(parse))
+    }
     log(parse)
 
-    printParse(parse)
+    // lexical pass
+    {
+      var table = {}
+      symbleTable(parse,table)
+      log(table)
+    }
 
-    parse = replace(parse, "'","'", 'str')
-    parse = replace(parse, '"','"', 'str')
-    parse = replace(parse, '`','`', 'str')
-    parse = replace(parse, '//', '\n', 'com')
-    parse = replace(parse, '/*', '*/', 'com')
-    parse = replace(parse, '(',')', '()', true)
-    parse = replace(parse, '[',']', '[]', true)
-    parse = replace(parse, '{','}', '{}', true)
-    log(parse)
 
     return function() {
 
@@ -603,16 +940,14 @@ PT.fcc = function() {
   }
 }()
 
-var a = '1234'
 
 var fun = PT.fcc(`
-  vec3 main(vec3 a, vec3 b, vec1 c, mat3_4 m) {
-    // this is a comment
-    str s = 'asdf'
-    /* this console.log(require('util').inspect(, { depth: null }));
-    as big as I want it to be */
-    vec3 k = [1,0,0]
-    return (a + (c b)) // this is the ans
+  vec3 main(vec3 a, vec3 b) {
+    vec3 c = b - a 4 + 3 4
+    var t = ++(--3)-- ++(3--) ^ ~3 + !34 ? 3 : 3
+    halpab == 13 >= 3 < 10 > 3 <= 1 != 3
+
+    return |c|
   }
 `)
 log(fun)
