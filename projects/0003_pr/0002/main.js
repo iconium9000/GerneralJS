f7c43a6cbea1622b86841ef34b059f97e80bb2d6
John FitzGerald <iconium9000@gmail.com>
2018-05-03 14:13:44 -0700

    v0.8.7.8

--- projects/0003_pr/0002/main.js
+++ projects/0003_pr/0002/main.js
@@ -2,70 +2,92 @@ log = console.log
 err = console.error
 
 var mem_val = {}
-var mem_loc = {
-  '@0000':'$zero','@0001':'$temp1','@0002':'$temp2','@0003':'$temp3',
-  '@0004':'$var0','@0005':'$var1','@0006':'$var2','@0007':'$var3',
-  '@0008':'$ret0','@0009':'$ret1','@000A':'$arg0','@000B':'$arg1',
-  '@000C':'$sp','@000D':'$ra','@000E':'$pc','@000F':'$ni',
-  '@ff00': '$float',
-  '@fd00': '$disk',
-  '@fc00': '$alu',
-  '@4000': '$start'
-}
-var insts = ['or','xor','and','nor','nand',]
-var mem_vloc = {}
-{
-  for (var i in mem_loc) {
-    var loc = get_loc(i)
-    var val = mem_loc[i]
-    delete mem_loc[i]
-    mem_loc[loc] = val
-  }
-  for (var i in mem_loc) mem_vloc[mem_loc[i]] = i
-}
+var prog = `9105 9203 8312 0000`.split(' ')
+for(var i=0;i<prog.length;++i)
+  mem_val[to_loc(i+0x4000)]=prog[i]
+
+var mem_usat = [0,'0000']
+var mem_inst = '0000'
+var mem_ufun = null
 
 var mem_fun = {
-  '$zero': val => 'zero',
-  '$alu': val => {
-    var loc = get_loc(val)
-    err('err',val,loc)
-    return '$zero'
-  }
+  'f0a0': [
+    [0,'f0bb','bf01'],
+    [2,(l,v)=>[0,'f0b'+v[0],v]],
+    [1,'f0f0','000f'],
+    [1,'f0a0','f0e0']
+  ],
+  'f0bb': [
+    [2,(l,v)=>[0,'f0e0','00'+v[2]+v[3]]],
+    [2,(l,v)=>[3,(a,b)=>a+b,'000'+v[1],'000'+v[1],'f0e0']]
+  ],
+  'f0af': [
+    [1,'000f','f0af'],
+    [1,'f0f0','000f'],
+    [1,'f0a0','f0e0']
+  ],
 }
-function get_loc(val) {
-  if (val[0]=='@') {
-    var ret = '@'
-    for (var i=1;i<5;++i) {
-      var l = val[i] || '0'
-      var c = l.charCodeAt(0)
-      if (57>=c&&c>=48) ret += l
-      else if (102>=c&&c>=97) ret += l
-      else if (70>=c&&c>=65) ret += String.fromCharCode(c+32)
-      else ret += '0'
-    }
-    return ret
-  }
-  if (val[0]=='#') {
-    val = val.slice(1).split(' ')
-  }
-  if (val[0]=='$'&&mem_vloc[val]) return mem_vloc[val]
-  throw `bad val '${val}'`
-}
-function get_val(loc) {
-  err('get_val',loc)
-  return mem_loc[loc] || loc
-}
-function mem_rd(loc) {
-  return mem_val[get_loc(loc)] || '$zero'
-}
-function mem_wt(loc,val) {
-  loc = get_loc(loc)
-  val = get_loc(val)
-  val = get_val(val)
-  loc = get_val(loc)
-  var fun = mem_fun[loc]
-  return mem_val[loc] = fun ? get_val(get_loc(fun(val))) : val
+for (var i=0xf0f0;i<0xf100;++i)
+  mem_fun[to_loc(i)]=[[2,(l,v)=>[1,'f0e'+l[3],v]]]
+
+var mem_sat = {
+
 }
 
-mem_wt('$alu','$start')
-log(mem_val)
+function to_reg(loc,idx) {
+  return '000'+loc[idx]
+}
+function mem_rd(loc) {
+  return mem_val[loc]||'0000'
+}
+function get_reg(loc,idx) {
+  return mem_rd(to_reg(loc,idx))
+}
+function to_int(loc) {
+  var int = parseInt('0x'+loc)
+  if (int<0x8000) return int
+  return int-0x10000
+}
+function to_loc(int) {
+  if (int>0xffff) int = int % 0x10000
+  if (int<-0x8000) int = int % 0x8000
+  if (int< 0) int = 0xffff+int+1
+  return ('0000'+int.toString(16)).slice(-4)
+}
+
+function call(loc,val) {
+  mem_usat = [0,mem_inst]
+  if (mem_fun[loc]) mem_sat[loc] = mem_usat
+  mem_val[loc] = val
+  mem_inst = loc
+  mem_ufun = null
+}
+var sanity = 0
+function boot() {
+  while (true) {
+    if (sanity++>0x100) return `sanity`
+
+    var sat = mem_sat[mem_inst] || mem_usat
+    if (!mem_ufun && mem_fun[mem_inst]) mem_ufun = mem_fun[mem_inst][sat[0]++]
+    log(sanity,mem_inst,mem_rd(mem_inst),sat,mem_ufun)
+    var fun = mem_ufun
+    // if (!fun) err(sat[1])
+    if (!fun) mem_inst = sat[1]
+    else if (fun[0]==0) call(fun[1],fun[2])
+    else if (fun[0]==1) call(fun[1],mem_rd(fun[2]))
+    else if (fun[0]==2) mem_ufun = fun[1](mem_inst,mem_rd(mem_inst))
+    else if (fun[0]==3) {
+      var ra = to_int(mem_rd(fun[3]))
+      var rb = to_int(mem_rd(fun[4]))
+      mem_ufun = [0,fun[2],to_loc(fun[1](ra,rb))]
+    }
+    else break
+  }
+}
+
+call('f0af','4000')
+err(boot())
+log(`mem_inst`,mem_inst)
+log(`mem_val`,mem_val)
+log(`mem_sat`,mem_sat)
+log(`mem_usat`,mem_usat)
