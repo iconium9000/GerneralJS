37ebf9dd517b4e6adfd244c3f2323fa2e3a35c64
John FitzGerald <iconium9000@gmail.com>
2018-09-27 09:56:10 -0700

    merge #3

--- projects/0004_mg/0003/game.js
+++ projects/0004_mg/0003/game.js
@@ -18,100 +18,136 @@ GAME_CLNT_INIT = () => {
 }
 
 GAME_TICK = () => {
+  var g = USR_IO_DSPLY.g
+  var wh = USR_IO_DSPLY.wh
+
 
 }
 
 // -----------------------------------------------------------------------------
-// Exceptions
-// -----------------------------------------------------------------------------
-DEF_ERR = 0
-BAD_USR = ++DEF_ERR
-BAD_PRG = ++DEF_ERR
-USED_NODE = ++DEF_ERR
-
-// -----------------------------------------------------------------------------
-// Modes
-// -----------------------------------------------------------------------------
-MODE_ID = 0
-WALL_MODE = ++MODE_ID
-DOOR_MODE = ++MODE_ID
-PORTAL_MODE = ++MODE_ID
-HANDLE_MODE = ++MODE_ID
-DRIVER_MODE = ++MODE_ID
-KEY_MODE = ++MODE_ID
-
-// -----------------------------------------------------------------------------
-// Overview
-// -----------------------------------------------------------------------------
 SIDS = 0
-EDITOR_CONTROLLER = false
-GAME_CONTROLLER = true
-
-NODE_RADIUS = 30
-
-// -----------------------------------------------------------------------------
-// Level Data
-// -----------------------------------------------------------------------------
-LEVELS = []
-SEL_LEVEL = null
+LEVELS = {}
 SEL_NODE = null
-SEL_PIECE = null
+SEL_PEICE = null
+SEL_LEVEL = null
+MODE = 'WALL'
+
+NODE_RADIUS = 15
+
+new_level()
+
+// -----------------------------------------------------------------------------
+
+function new_level() {
+  SEL_LEVEL = {
+    sid: ++SIDS,
+    nodes: {},
+    walls: {},
+    portals: {},
+    keys: {},
+    drivers: {}
+  }
+}
 
 function get_node(point) {
   for (var i in SEL_LEVEL.nodes) {
     var node = SEL_LEVEL.nodes[i]
-    if (NODE_RADIUS > PT.dist(point,node.point)) {
+    if (NODE_RADIUS * 2 > PT.dist(node.point,point)) {
       return node
     }
   }
   return null
 }
 
-function set_gate(node) {
+function check_handle(src_node,node) {
+  if (src_node == node) return true
+  else if (!src_node) return false
+  else return check_handle(node.src_node,node)
+}
+
+function set_gate(node,gate) {
 
 }
 
-
-function set_mode(mode,node) {
-  if (node.src_node) {
-    switch (mode) {
-      case WALL_MODE:
-
-      case DOOR_MODE:
-
-      case PORTAL_MODE:
-      case HANDLE_MODE:
-
+function new_wall(src_node,node) {
+  var wall = node.walls[src_node.sid]
+  if (wall) return wall
+  else {
+    var wall = {
+      sid: ++SIDS,
+      src_node: src_node,
+      node: node,
     }
-  }
-  else if (mode == PORTAL_MODE) {
-    
+    SEL_LEVEL.walls[wall.sids] = wall
+    src_node.walls[node.sid] = { wall: wall, node: node }
+    node.walls[src_node.sid] = { wall: wall, node: src_node }
+    return wall
   }
 }
 
-function new_node(point,src_node,mode) {
-  var node = get_node(point)
-  if (node) {
-    node.src_node = src_node
-    return set_mode(mode,node)
+function kill_wall(src_node,node) {
+  var wall = node.walls[src_node.sid]
+  if (wall) {
+    delete SEL_LEVEL.walls[wall.sid]
+    delete node.walls[src_node.sid]
+    delete src_node.walls[node.sid]
   }
-  var node = {
+}
+function killall_walls(node) {
+
+}
+
+function new_node(point,src_node) {
+  var node = get_node(point) || {
     sid: ++SIDS,
-    point: PT.copy(point),
-    src_node: src_node,
-    peice_target: false,
+    peice_node: null,
+    square: null,
+    key: null,
+    driver: null,
+    src_node: null,
     handles: {},
-    doors: {},
     walls: {}
   }
   SEL_LEVEL.nodes[node.sid] = node
-  return set_mode(mode,src_node,node)
+  if (!src_node) {
+    if (MODE == 'PORTAL') {
+
+    }
+    return node
+  }
+
+  switch (MODE) {
+    case 'WALL':
+      node.peice_node = false
+      var wall = new_wall(src_node,node)
+      wall.gate = null
+      set_gate(node,null)
+      set_gate(src_node,null)
+      return node
+    case 'DOOR':
+      node.peice_node = false
+      var wall = new_wall(src_node,node)
+      wall.gate = true
+      set_gate(node,null)
+      return node
+    case 'HANDLE':
+      kill_wall(src_node,node)
+
+      if (check_handle(src_node,node)) return node
+      if (node.src_node) {
+        delete node.src_node.handles[node.sid]
+        set_gate(node.src_node,null)
+      }
+      node.src_node = src_node
+      src_node.handles[node.sid] = node
+      node.peice_node = true
+      for (var i in node.walls) {
+        node.peice_node = false
+        break
+      }
+      set_gate(node.src_node,null)
+      return node
+    case 'PORTAL':
+
+  }
 }
-
-// -----------------------------------------------------------------------------
-// Editor Controller
-// -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// Game Controller
-// -----------------------------------------------------------------------------
