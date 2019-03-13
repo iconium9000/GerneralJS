2222e04f7910e3e0dffc4d46dbe8be9b4ae8249e
John FitzGerald <iconium9000@gmail.com>
2019-03-13 16:53:57 -0700

    Create assemble_scripts.js

---
+++ screens/assemble_scripts.js
@@ -0,0 +1,27 @@
+var fs = require('fs')
+var log = console.log
+
+
+var games = {
+  3000: [
+    'menu_3000',
+    '05_games/00_menu',
+    3000
+  ],
+  3001: [
+    'blockade_3001',
+    '05_games/01_blockade',
+    3001
+  ],
+  3005: [
+    'knifeline',
+    '05_games/05_knifeline',
+    3005
+  ],
+}
+
+function write_file(file_name, obj) {
+  fs.writeFileSync(file_name, JSON.stringify(obj), 'utf8', ()=>{})
+}
+
+log(process.argv)
