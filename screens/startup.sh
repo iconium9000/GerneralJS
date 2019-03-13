7eb67180328bfe5aee7823b131e117c330883c4c
John FitzGerald <iconium9000@gmail.com>
2019-03-13 15:12:50 -0700

    Update startup.sh

--- screens/startup.sh
+++ screens/startup.sh
@@ -2,9 +2,11 @@
 #echo BASH SETUP GENERALJS NODE SERVERS
 
 {
+	chmod +x ~/Github/GeneralJS/screens/3001_blockade.sh
 	screen -d -m -S 3001 ~/Github/GeneralJS/screens/3001_blockade.sh
 }
 {
+	chmod +x ~/Github/GeneralJS/screens/3005_knifeline.sh
 	screen -d -m -S 3005 ~/Github/GeneralJS/screens/3005_knifeline.sh
 }
 
