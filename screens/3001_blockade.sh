60c71e3594699940b00f9518dcec118d5518e3df
John FitzGerald <iconium9000@gmail.com>
2019-03-13 18:34:15 -0700

    cleaning up screens directory

--- screens/3001_blockade.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-#3001_blockade.sh
-
-echo starting blockade on port 3001
-cd ~/Github/GeneralJS/
-node app 05_games/01_blockade 3001
--- screens/3005_knifeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-#3005_knifeline.sh
-
-echo starting knifeline on port 3005...
-
-cd ~/Github/GeneralJS/
-node app 05_games/05_knifeline 3005
