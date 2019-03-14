b46a1065944bbab13c2cd4bd12e1efcc723089e4
John FitzGerald <iconium9000@gmail.com>
2019-03-13 19:06:53 -0700

    Update bootstrap_linux.sh

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,3 +1,55 @@
 #!/bin/bash -e
 
-echo "$PATH"
+GENERALJS=$(pwd)
+
+echo
+echo --GO to "$GENERALJS"
+
+cd $GENERALJS
+
+{
+  echo
+  echo --UPDATE git repository
+  git reset --hard origin/master
+  git gc
+  git fetch origin master
+}
+
+{
+  echo
+  echo --CREATE assembly instruections with nodeJS
+  node screens/assemble_scripts.js $GENERALJS
+}
+
+cd screens
+
+{
+  echo
+  echo --CLEAR previous screens
+  echo why not do it 3 times??
+
+  chmod +x clear_all.sh
+  echo -clear_all try 1
+  ./clear_all.sh
+  echo -clear_all try 2
+  ./clear_all.sh
+  echo -clear_all try 3
+  ./clear_all.sh
+
+  rm clear_all.sh
+}
+
+{
+  echo
+  echo --START new servers
+
+  chmod +x startup.sh
+  ./startup.sh
+  rm startup.sh
+}
+
+{
+  echo
+  echo
+  echo --COMPLETE!
+}
