89916bd240ee9f20fede94fb429195ae79f9dbd3
John FitzGerald <iconium9000@gmail.com>
2019-03-13 18:12:27 -0700

    Update assemble_scripts.js

--- screens/assemble_scripts.js
+++ screens/assemble_scripts.js
@@ -5,22 +5,19 @@ var directory = process.argv[2]
 
 log(`init ${directory}screens/assemble_scripts.js`)
 
-var games = {
-  3000: [
-    'menu_3000',
-    '05_games/00_menu',
-    3000
-  ],
-  3001: [
-    'blockade_3001',
-    '05_games/01_blockade',
-    3001
-  ],
-  3005: [
-    'knifeline',
-    '05_games/05_knifeline',
-    3005
-  ],
+var projects = {
+  3000:{
+    name: 'menu_3000',
+    proj: '05_games/00_menu'
+  },
+  3001: {
+    name: 'blockade_3001',
+    proj: '05_games/01_blockade',
+  },
+  3005: {
+    name: 'knifeline_3005',
+    proj: '05_games/05_knifeline',
+  },
 }
 
 function write_file(file_name, txt) {
@@ -29,5 +26,17 @@ function write_file(file_name, txt) {
 
 var bash_start = '#!/bin/bash -e\n'
 
-var clear_all_txt = `${bash_start}#clear_all.sh`
+var clear_all_txt = `${bash_start}#clear_all.sh\n`
+
+for (var port in projects) {
+  var project = projects[port]
+
+  clear_all_txt += `\t{ #try
+    screen -X -S ${project.name} quit &&
+    echo closed ${project.name} screen
+  } || { #catch
+    echo ${project.name} screen already closed
+  }\n\n`
+}
+
 write_file('screens/clear_all.sh', clear_all_txt)
